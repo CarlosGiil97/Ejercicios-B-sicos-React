eb0a8faad8ec58e249a6f5857dc552c1e34ae673
unknown <carlosgilterriente@gmail.com>
2021-06-24 20:20:08 +0200

    Carpeta componentes

---
+++ src/components/CrudApp.js
@@ -0,0 +1,12 @@
+//he usado el atajo rafce para crear automaticamente el import y el const CrudApp
+import React from 'react'
+
+const CrudApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CrudApp
