4f02e3a9f4e302383b01c2adf736044d5391998b
unknown <carlosgilterriente@gmail.com>
2021-06-29 18:37:10 +0200

    Ejercicio 2 - CRUD con Api - JSON Server

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import './App.css';
 import CrudApp from './components/CrudApp';
+import CrudApi from './components/CrudApi';
 
 function App() {
   return (
     <div>
       <h2>Ejercicios React Básicos</h2>
-      <CrudApp />
+      <CrudApi />
       <hr />
+      <CrudApp />
     </div>
   );
 }
