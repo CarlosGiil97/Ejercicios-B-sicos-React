6bc06e03686416ce66541d3731e2a419dcee2bbb
unknown <carlosgilterriente@gmail.com>
2021-06-29 19:27:47 +0200

    Opciones del helper que hará las peticiones realizadas

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -1,11 +1,61 @@
-import React from 'react'
+//he usado el atajo rafce para crear automaticamente el import y el const CrudApi
+import React,{useState} from 'react';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+
+
 
 const CrudApi = () => {
+    const [db, setDb] = useState([]);
+    const [dataToEdit, setDataToEdit] = useState(null);
+
+        //funcion que se encargará de insertar nueva info en la BD
+    const createData = (data) => {
+            //manera de generar un ID para el componente único
+            data.id = Date.now();
+            setDb([...db,data]);
+            
+    }
+//funcion que va a actualizar la info de la BD
+const updateData = (data) => {
+    let newData = db.map(el => el.id === data.id ? data : el);
+    setDb(newData);
+}
+
+//funcion que va a eliminar info de la BD
+const deleteData = (id) => {
+    let isDelete = window.confirm (`¿Estas seguro de eliminar el registro con el id '${id}'?`)
+    console.log(isDelete);
+   
+    if(isDelete){
+        console.log('DB ANTIGUA:')
+        console.log(db);
+        let newData = db.filter((el) => el.id !== id);
+        setDb(newData);
+        console.log('DB NUEVA:')
+        console.log(db);
+    }
+}
     return (
-        <div>
-            <h2>Crud Api</h2>
-        </div>
+        <>
+            <h2>CRUD APP</h2>
+            <article className="grid-1-2">
+            <CrudForm 
+            createData={createData} 
+            updateData={updateData} 
+            dataToEdit={dataToEdit} 
+            setDataToEdit={setDataToEdit}/>
+
+            <CrudTable 
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData} 
+            />
+            </article>
+            
+        </>
     )
 }
 
-export default CrudApi
+export default CrudApi;
---
+++ src/helpers/helpHttp.js
@@ -0,0 +1,37 @@
+export const helpHttp = () => {
+    //no se le pone el eport default porque no se le va a cambiar el nombre cuando se importe en otro fichero
+    //el endpoint es la ruta donde queremos hacer la petición
+    const customFetch = (endpoint,options) => {
+        const defaultHeader = {
+            accept:"application/json",
+        };
+
+        const controller = new AbortController();
+        //añadir la propiedad controller-> manejador de errores para cuando el endpoint no responde
+        options.signal = controller.signal;
+        //comprobar el estado que tiene la propiedad method
+        options.method = options.method || "GET";
+
+        //si el usuario manda cabeceras, se añaden las defaultheaders .En caso contrario, que ponga las defaultHeaders
+        options.headers = options.headers ? {...defaultHeader,...options.headers} : defaultHeader
+    
+        //el cuerpo hay que convertirlo a cadena cuando el usuario mande datos
+        options.body = JSON.stringify(options.body) || false;
+
+        //si el options body viene vacio se elimina , ya que no se puede enviar vacio al hacer peticiones
+        if(!options.body) delete options.body;
+    }
+
+    const get = () => {}
+
+    const post  = () => {}
+
+    const put  = () => {}
+
+    const del  = () => {}
+
+    return{
+        get,post,put,del
+    }
+
+}
